29698dd79b33f1b7a7424f95e6daffecd5654932
Isabella Meira Faria <isabellameiraf@gmail.com>
2020-11-29 23:15:38 -0300

    adicionando leitura

--- Esteira.cs
+++ Esteira.cs
@@ -5,116 +5,119 @@ namespace Grafica
 {
     class Esteira
     {
-		private static  int TempoDia = 32400;
-		private static  int TempoMeioDia = 14400;
-		private static  int TempoDepoisMeioDia = 18000;
-		private static float tempoAtualAntesMeioDia = 0;
-		private static float tempoAtualDepoisMeioDia = 0;
+        private static int TempoDia = 32400;
+        private static int TempoMeioDia = 14400;
+        private static int TempoDepoisMeioDia = 18000;
+        private static float tempoAtualAntesMeioDia = 0;
+        private static float tempoAtualDepoisMeioDia = 0;
 
-		public static void Executar(int quantidade, List<Pedidos> pedidos)
-		{
+        public static void Executar(int quantidade, List<Pedidos> pedidos)
+        {
 
-			for (int index = 0; index < pedidos.size(); index++)
-			{
-				if (pedidos.get(index).Prazo > 0 && pedidos.get(index).Prazo * 60 <= TempoMeioDia)
-				{
-					pedidos.get(index).FlagMeioDia = 1;
-					pedidos.get(index).CalcularTempoProducao();
-				}
-				else
-				{
-					pedidos.get(index).CalcularTempoProducao();
-				}
-			}
+            // for (int index = 0; index < pedidos.size(); index++)
+            // {
+            //     if (pedidos(index).Prazo > 0 && pedidos.get(index).Prazo * 60 <= TempoMeioDia)
+            //     {
+            //         pedidos.get(index).FlagMeioDia = 1;
+            //         pedidos.get(index).CalcularTempoProducao();
+            //     }
+            //     else
+            //     {
+            //         pedidos.get(index).CalcularTempoProducao();
+            //     }
+            // }
 
-			Collections.sort(pedidos, new IComparable<Pedidos>()
-			{
+            //             Collections.sort(pedidos, new IComparable<Pedidos>()
+            //             {
 
-				public int compare(Pedidos o1, Pedidos o2)
-			   {
-					if (o1.GetPrazo != 0 && o2.Prazo != 0)
-					{
-						if (o1.Prazo < o2.Prazo)
-						{
-							return -1;
-						}
-						if (o1.Prazo > o2.Prazo)
-						{
-							return 1;
-						}
-						else return 0;
-					}
-					if (o1.Prazo != 0 && o2.Prazo == 0)
-					{
-						return -1;
-					}
-					if (o1.Prazo == 0 && o2.Prazo != 0)
-					{
-						return 1;
-					}
 
-					if (o1.Prazo == o2.Prazo)
-					{
-						if (o1.TempoProducao > o2.TempoProducao)
-						{
-							return 1;
-						}
-						if (o1.TempoProducao < o2.TempoProducao)
-						{
-							return -1;
-						}
-						else return 0;
-					}
-					return 0;
-				}
-		});
-		 
-		 int quantidadeProduzida = 0, produtosAntesMeioDia = 0;
-		int foraPrazo = 0;
-		int tempo = 0; 
-			for(int index = 0; index<produto.size() ; index ++ ) 
-			{
-				tempo += produto.get(index).TempoProducao;
-				if(!(tempo > TempoDia)) {
-					if(tempo <= TempoMeioDia) 
-					{
-						produtosAntesMeioDia ++;
-					}
-				if(produto.get(index).Prazo > 0 && tempo > produto.get(index).Prazo* 60)
-				{
-					foraPrazo ++;
-				}
-                       quantidadeProduzida++;
-				}
-			}
-			System.out.println("Fora do Prazo :" + foraPrazo + " Produzidos: " + quantidadeProduzida + " Antes do meio dia:" + produtosAntesMeioDia);
-		}
-		
+            //                 public int compare(Pedidos o1, Pedidos o2)
+            //             {
+            //                 if (o1.GetPrazo != 0 && o2.Prazo != 0)
+            //                 {
+            //                     if (o1.Prazo < o2.Prazo)
+            //                     {
+            //                         return -1;
+            //                     }
+            //                     if (o1.Prazo > o2.Prazo)
+            //                     {
+            //                         return 1;
+            //                     }
+            //                     else return 0;
+            //                 }
+            //                 if (o1.Prazo != 0 && o2.Prazo == 0)
+            //                 {
+            //                     return -1;
+            //                 }
+            //                 if (o1.Prazo == 0 && o2.Prazo != 0)
+            //                 {
+            //                     return 1;
+            //                 }
 
-	
-	public static void Fifo(int quantidade, List<Produto> produto)
-{
-	int quantidadeProduzida = 0, produtosAntesMeioDia = 0;
-	int foraPrazo = 0;
-	int tempo = 0;
-	for (int index = 0; index < produto.size(); index++)
-	{
-		produto.get(index).CalcularTempoProducao();
-		tempo += produto.get(index).TempoProducao;
-		if (!(tempo > TempoDia))
-		{
-			if (tempo <= TempoMeioDia)
-			{
-				produtosAntesMeioDia++;
-			}
-			if (produto.get(index).Prazo > 0 && tempo > produto.get(index).Prazo * 60)
-			{
-				foraPrazo++;
-			}
-			quantidadeProduzida++;
-		}
-	}
-	System.out.println("Fora do Prazo :" + foraPrazo + " Produzidos: " + quantidadeProduzida + " Antes do meio dia:" + produtosAntesMeioDia);
-}
+            //                 if (o1.Prazo == o2.Prazo)
+            //                 {
+            //                     if (o1.TempoProducao > o2.TempoProducao)
+            //                     {
+            //                         return 1;
+            //                     }
+            //                     if (o1.TempoProducao < o2.TempoProducao)
+            //                     {
+            //                         return -1;
+            //                     }
+            //                     else return 0;
+            //                 }
+            //                 return 0;
+            //             }
+            //         });
+
+            // 		 int quantidadeProduzida = 0, produtosAntesMeioDia = 0;
+            //         int foraPrazo = 0;
+            //         int tempo = 0; 
+            // 			for(int index = 0; index<produto.size() ; index ++ ) 
+            // 			{
+            // 				tempo += produto.get(index).TempoProducao;
+            // 				if(!(tempo > TempoDia)) {
+            // 					if(tempo <= TempoMeioDia) 
+            // 					{
+            // 						produtosAntesMeioDia ++;
+            // 					}
+            // 				if(produto.get(index).Prazo > 0 && tempo > produto.get(index).Prazo* 60)
+            // 				{
+            // 					foraPrazo ++;
+            // 				}
+            // quantidadeProduzida++;
+            // 				}
+            // 			}
+            // 			System.out.println("Fora do Prazo :" + foraPrazo + " Produzidos: " + quantidadeProduzida + " Antes do meio dia:" + produtosAntesMeioDia);
+            // 		}
+
+
+
+            // 	public static void Fifo(int quantidade, List<Produto> produto)
+            // {
+            //     int quantidadeProduzida = 0, produtosAntesMeioDia = 0;
+            //     int foraPrazo = 0;
+            //     int tempo = 0;
+            //     for (int index = 0; index < produto.size(); index++)
+            //     {
+            //         produto.get(index).CalcularTempoProducao();
+            //         tempo += produto.get(index).TempoProducao;
+            //         if (!(tempo > TempoDia))
+            //         {
+            //             if (tempo <= TempoMeioDia)
+            //             {
+            //                 produtosAntesMeioDia++;
+            //             }
+            //             if (produto.get(index).Prazo > 0 && tempo > produto.get(index).Prazo * 60)
+            //             {
+            //                 foraPrazo++;
+            //             }
+            //             quantidadeProduzida++;
+            //         }
+            //     }
+            //     System.out.println("Fora do Prazo :" + foraPrazo + " Produzidos: " + quantidadeProduzida + " Antes do meio dia:" + produtosAntesMeioDia);
+            // }
+            //     }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Grafica
 {
@@ -7,32 +8,44 @@ namespace Grafica
     {
         static void Main(string[] args)
         {
-            // string[] lines = System.IO.File.ReadAllLines(@"C:\Users\amanda.carvalho\source\repos\Grafica\DadosSo.txt");
-            string[] lines = System.IO.File.ReadAllLines(@"C:\DadosSo.txt");
+            // string path = @"C:\Users\amanda.carvalho\source\repos\Grafica\DadosSo.txt";
+            string path = @"C:\DadosSo.txt";
+            string[] lines;
 
-            Console.WriteLine("Contents of WriteLines2.txt = ");
-            
-            List<Pedidos> listarPedidos = new List<Pedidos>();
-            int i = 0;
-
-            foreach (string line in lines)
+            if (File.Exists(path))
             {
-                
-                if (i == 0)
-                {
-                    int quantidade = Convert.ToInt32(line);
-                }
-                else
-                {
-                    String[] campos = line.Split(';');
-
-                    listarPedidos.Add(new Pedidos(campos[0], Convert.ToInt32(campos[1]), Convert.ToInt32(campos[2]),
-                           Convert.ToInt32(campos[3])));
-                }
-
-                i++;
+                Console.WriteLine("Existe");
+                lines = File.ReadAllLines(path);
+                Console.WriteLine(lines.Length);
 
             }
+            else
+            {
+                Console.WriteLine("Não Existe");
+            }
+
+            List<Pedidos> listarPedidos = new List<Pedidos>();
+
+            // int i = 0;
+
+            // foreach (string line in lines)
+            // {
+
+            //     if (i == 0)
+            //     {
+            //         int quantidade = Convert.ToInt32(line);
+            //     }
+            //     else
+            //     {
+            //         String[] campos = line.Split(';');
+
+            //         listarPedidos.Add(new Pedidos(campos[0], Convert.ToInt32(campos[1]), Convert.ToInt32(campos[2]),
+            //                Convert.ToInt32(campos[3])));
+            //     }
+
+            //     i++;
+
+            // }
 
         }
     }
